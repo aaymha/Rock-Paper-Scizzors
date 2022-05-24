30e6cd6fe41739de61f3b0b87201eeff36bb3dd0
Aaymha <szymon7710@icloud.com>
2022-05-24 23:32:03 +0200

    eh

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ function computerPlay() {
     return ran;
   }
   
-  computerPlay();
+function userPlay() {
+    let zap1 = prompt('Make a choice'); 
+}
 
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === 'Rock') {
+        if(computerSelection === 'Rock') {
+            alert('Tie!')
+        } else if(computerSelection == 'Scizzors') {
+            alert('You won!')
+        } else {
+            alert('You')
+        }
+    }
+  }
+  
+  const playerSelection = userPlay();
+  const computerSelection = computerPlay();
