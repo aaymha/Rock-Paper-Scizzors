b938a7fd0b2b79c4281ba776ed75da22e8cb6394
Aaymha <szymon7710@icloud.com>
2022-05-24 23:54:45 +0200

    s

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function computerPlay() {
-    let tab = new Array('Rock', 'Paper', 'Scizzors');
+    let tab = ['Rock', 'Paper', 'Scizzors'];
     let ran = (tab[Math.floor(Math.random() * tab.length)]);
     return ran;
   }
   
 function userPlay() {
-    let zap1 = prompt('Make a choice'); 
+    let pro = prompt('Start fight!')
 }
 
+userPlay();
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'Rock') {
-        if(computerSelection === 'Rock') {
-            alert('Tie!')
-        } else if(computerSelection == 'Scizzors') {
-            alert('You won!')
-        } else {
-            alert('You')
-        }
-    }
+    
   }
   
   const playerSelection = userPlay();
