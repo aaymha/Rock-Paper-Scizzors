0e3d73f4e22c6c48e0f979b35af904e972084be9
Aaymha <szymon7710@icloud.com>
2022-05-24 21:00:53 +0200

    nice

---
+++ script.js
@@ -0,0 +1,7 @@
+function computerPlay() {
+    let tab = new Array('Rock', 'Paper', 'Scizzors');
+    let ran = (tab[Math.floor(Math.random() * tab.length)]);
+    return ran;
+  }
+  
+  computerPlay();
