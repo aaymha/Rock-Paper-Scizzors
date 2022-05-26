01bf4d4cac20cffbb421698ea929a696737df7d8
Aaymha <szymon7710@icloud.com>
2022-05-26 14:50:11 +0200

    hikujgui

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function computerPlay() {
   let tab = ['Rock', 'Paper', 'Scizzors'];
   let ran = (tab[Math.floor(Math.random() * tab.length)]);
@@ -14,24 +13,27 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay();
   playerSelection = userPlay();
 
-  if(playerSelection === 'Rock' && computerSelection === 'Rock') {
-    let a = alert("Draw");
-  } else if(playerSelection === 'Scizzors') {
-    let b = alert("YOU LOST")
-  } else if(playerSelection === 'Paper') {
-    let go = alert("YOU WON")
-  } else if(playerSelection === 'Paper' && computerSelection === 'Paper') {
-    let c = alert("Draw");
-  } else if(playerSelection === 'Scizzors') {
-    let d = alert("YOU WON")
-  } else if(playerSelection === 'Rock') {
-    let e = alert("YOU LOST")
-  } else if(playerSelection === 'Scizzors' && computerSelection === 'Scizzors') {
-    let f = alert("Draw");
-  } else if(playerSelection === 'Rock') {
-    let g = alert("YOU WON")
-  } else if(playerSelection === 'Paper') {
-    let h = alert("YOU LOST")
+  if(playerSelection === computerSelection) {
+    alert('Draw');
   }
+  if(playerSelection === 'Rock' && computerSelection === 'Paper') {
+    alert('You lost!')
+  }
+  if(playerSelection === 'Rock' && computerSelection === 'Scizzors') {
+    alert('You won!')
+  }
+  if(playerSelection === 'Paper' && computerSelection === 'Scizzors') {
+    alert('You lost!')
+  }
+  if(playerSelection === 'Paper' && computerSelection === 'Rock') {
+    alert('You won!')
+  }
+  if(playerSelection === 'Scizzors' && computerSelection === 'Rock') {
+    alert('You lost!')
+  }
+  if(playerSelection === 'Scizzors' && computerSelection === 'Paper') {
+    alert('You won!')
+  }
+  return playerSelection, computerSelection;
 }
 playRound();
