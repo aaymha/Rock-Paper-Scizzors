0266ad34984e5c5e4aa6f9330b9486af0e08c8a2
Aaymha <szymon7710@icloud.com>
2022-05-26 14:18:41 +0200

    S

--- script.js
+++ script.js
@@ -16,22 +16,22 @@ function playRound(playerSelection, computerSelection) {
 
   if(playerSelection === 'Rock' && computerSelection === 'Rock') {
     let a = alert("Draw");
-  } else if(computerSelection === 'Scizzors') {
-    let b = alert("YOU WON")
-  } else if(computerSelection === 'Paper') {
-    let go = alert("YOU LOST")
+  } else if(playerSelection === 'Scizzors') {
+    let b = alert("YOU LOST")
+  } else if(playerSelection === 'Paper') {
+    let go = alert("YOU WON")
   } else if(playerSelection === 'Paper' && computerSelection === 'Paper') {
     let c = alert("Draw");
-  } else if(computerSelection === 'Scizzors') {
-    let d = alert("YOU LOST")
-  } else if(computerSelection === 'Rock') {
-    let e = alert("YOU WON")
+  } else if(playerSelection === 'Scizzors') {
+    let d = alert("YOU WON")
+  } else if(playerSelection === 'Rock') {
+    let e = alert("YOU LOST")
   } else if(playerSelection === 'Scizzors' && computerSelection === 'Scizzors') {
     let f = alert("Draw");
-  } else if(computerSelection === 'Rock') {
-    let g = alert("YOU LOST")
-  } else if(computerSelection === 'Paper') {
-    let h = alert("YOU WON")
+  } else if(playerSelection === 'Rock') {
+    let g = alert("YOU WON")
+  } else if(playerSelection === 'Paper') {
+    let h = alert("YOU LOST")
   }
 }
 playRound();
