6135129b1c20e9b0bee0fc643269b9a148f05ddc
Aaymha <szymon7710@icloud.com>
2022-05-29 01:41:12 +0200

    sd

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-win1 = 'You won!';
-draw1 = 'Its a draw!';
-lose1 = 'You got fucked by multiple black men!';
 vicotry = 'You won the game!';
 defeat = 'You lost the game!';
+
+
 
 function computerPlay() {
   let tab = ['Rock', 'Paper', 'Scizzors'];
@@ -16,58 +15,37 @@ return van;
 }
 
 function playRound() {
-  const computerSelection = computerPlay();
-  const playerSelection = userPlay();
-
+  let computerSelection = computerPlay();
+  let playerSelection = userPlay();
   if(playerSelection === computerSelection) {
-    alert(draw1);
+    alert('Draw');
   }
   if(playerSelection === 'Rock' && computerSelection === 'Paper') {
-   alert(lose1)
+    alert('You got fucked by multiple black men!')
   }
   if(playerSelection === 'Rock' && computerSelection === 'Scizzors') {
-   alert(win1)
+    alert('You won!')
   }
   if(playerSelection === 'Paper' && computerSelection === 'Scizzors') {
-    alert(lose1)
+    alert('You got fucked by multiple black men!')
   }
   if(playerSelection === 'Paper' && computerSelection === 'Rock') {
-    alert(win1)
+    alert('You won!')
   }
   if(playerSelection === 'Scizzors' && computerSelection === 'Rock') {
-    alert(lose1)
+    alert('You got fucked by multiple black men!')
   }
   if(playerSelection === 'Scizzors' && computerSelection === 'Paper') {
-    alert(win1)
+    alert('You won!')
   }
+  
 }
 
 
 function game() {
-  let playerSelection;
-  let computerSelection;
-  let playerscore = 0;
-  let computerscore = 0;
   for (let i = 0; i < 5; i++) {
     playRound();
-    if(playerSelection = win1) {
-      console.log('You won this round!')
-      return playerscore++
-    } 
-    if(computerSelection = lose1) {
-      console.log('You lost the round!')
-      return computerscore++
-    }
-
-
-    if(playerscore = 5) {
-      console.log(vicotry);
-      return;
-    } else if(computerscore = 5) {
-      console.log(defeat);
-    }
   }
 }
 
 game();
-
